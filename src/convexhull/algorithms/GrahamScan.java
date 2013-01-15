af35c414082ef26982b0983a99264394d02509a2
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-01-15 18:23:45 +0200

    algorithm bases

---
+++ src/convexhull/algorithms/GrahamScan.java
@@ -0,0 +1,16 @@
+package convexhull.algorithms;
+
+import java.awt.geom.Point2D.Double;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Heikki Haapala
+ */
+public class GrahamScan implements Algorithm {
+
+    @Override
+    public ArrayList<Double> useAlgorithm(ArrayList<Double> points) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/convexhull/algorithms/QuickHull.java
@@ -0,0 +1,16 @@
+package convexhull.algorithms;
+
+import java.awt.geom.Point2D.Double;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aleksi Markkanen
+ */
+public class QuickHull implements Algorithm {
+
+    @Override
+    public ArrayList<Double> useAlgorithm(ArrayList<Double> points) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
