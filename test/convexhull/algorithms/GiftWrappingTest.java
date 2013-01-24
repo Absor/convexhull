e70a8aa5d8d0d7110b858716c182b5a5f92aba99
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-01-24 22:57:35 +0200

    changed algorithm tests to use two different point sets of 100 and 10000 points

--- test/convexhull/algorithms/GiftWrappingTest.java
+++ test/convexhull/algorithms/GiftWrappingTest.java
@@ -53,26 +53,33 @@ public class GiftWrappingTest {
      */
     @Test
     public void testUseAlgorithm() {
+        // test with 100 points
+        testAlgorithm("test100", "result100");
+        // test with 10000 points
+        testAlgorithm("test10000", "result10000");
+    }
+    
+    private void testAlgorithm(String inputfile, String resultfile) {
         System.out.println("useAlgorithm");
         // input set
         LinkedList input = null;
         try {
-            input = Helper.parseFile("testfile");
+            input = Helper.parseFile(inputfile);
         } catch (Exception ex) {
             fail("Could not parse input file.");
         }
         // known results
         LinkedList result = null;
         try {
-            result = Helper.parseFile("testresult");
+            result = Helper.parseFile(resultfile);
         } catch (Exception ex) {
             fail("Could not parse result file.");
         }
 
-        GiftWrapping giftAlgo = new GiftWrapping();
+        GiftWrapping algorithm = new GiftWrapping();
 
         // algorithm results
-        LinkedList algoResult = giftAlgo.useAlgorithm(input);
+        LinkedList algoResult = algorithm.useAlgorithm(input);
 
         if (!Helper.setsMatch(result, algoResult)) {
             fail("Algorithm returns wrong result.");
